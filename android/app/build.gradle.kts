42f9bb43319dcf02972e57541af7e19a689ce628
arknights251014-lab <arknights251014@gmail.com>
2026-06-25 00:51:25 +0900

    build.gradle.kts 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,23 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.infinite_buy_v4"
-    compileSdk = 34
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.infinite_buy_v4"
+
+        minSdk = 21
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0.0"
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -15,14 +26,6 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-    }
-
-    defaultConfig {
-        applicationId = "com.example.infinite_buy_v4"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
     }
 
     buildTypes {
