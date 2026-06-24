7dfed1036f37e816e0a2322e2ca0fa41f1fd314c
arknights251014-lab <arknights251014@gmail.com>
2026-06-24 22:15:55 +0900

    cycle_result.dart 업데이트

--- lib/models/cycle_result.dart
+++ lib/models/cycle_result.dart
@@ -1,19 +1,32 @@
-// order.dart
-enum OrderType {
-  locBuy,
-  locSell,
-  mocSell,
-  limitSell,
-}
+// cycle_result.dart
+import 'strategy_state.dart';
 
-class Order {
-  final OrderType type;
-  final double orderPrice;
-  final double qty;
+class CycleResult {
+  final String symbol;
+  final int splitCount;
+  final double targetProfit;
+  final double startCapital;
+  final double endAsset;
+  final double totalProfit;
+  final double returnRate;
+  final int investmentPeriod;
+  final double maxT;
+  final double maxQty;
+  final double maxExposure;
+  final CompletionReason completionReason;
 
-  const Order({
-    required this.type,
-    required this.orderPrice,
-    required this.qty,
+  const CycleResult({
+    required this.symbol,
+    required this.splitCount,
+    required this.targetProfit,
+    required this.startCapital,
+    required this.endAsset,
+    required this.totalProfit,
+    required this.returnRate,
+    required this.investmentPeriod,
+    required this.maxT,
+    required this.maxQty,
+    required this.maxExposure,
+    required this.completionReason,
   });
 }
